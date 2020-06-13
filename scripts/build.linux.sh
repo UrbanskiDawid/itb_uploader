c356d0c089dea11f5a26018e543d3d4b4ecf507e
Dawid <git@dawidurbanski.pl>
2020-06-13 13:33:13 +0100

    scripts

--- scripts/build.linux.sh
+++ scripts/build.linux.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+go get golang.org/x/crypto/ssh
 go build -o itb_uploader.linux -v .
--- scripts/build.windows.sh
+++ scripts/build.windows.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+go get golang.org/x/crypto/ssh
 GOOS=windows GOARCH=386 go build -o itb_uploader.exe -v .
