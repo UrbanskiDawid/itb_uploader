d2fa44a16def99715f1be67aea9ab0e16b9ae6e3
Dawid <git@dawidurbanski.pl>
2020-06-26 14:36:34 +0200

    fix SCP ?!

--- actions/scp.go
+++ actions/scp.go
@@ -30,7 +30,7 @@ func DownloadFileScp(localFileName string, conn *ssh.Client, remoteFile string) 
 		return errors.New("no SCP")
 	}
 
-	err := clientScp.ReceiveFile(localFileName, remoteFile)
+	err := clientScp.ReceiveFile(remoteFile, localFileName)
 	if err != nil {
 		logging.LogConsole(fmt.Sprintf("error: SCP fail while copying file", err))
 		return err
